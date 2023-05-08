a9842ccb701c394377d544e49a2bfcec94cddf2f
xxxBrandonxxx <108400190+xxxBrandonxxx@users.noreply.github.com>
2023-05-08 06:05:54 +0200

    Debugging reset button

--- resetButton.js
+++ resetButton.js
@@ -1,39 +1,45 @@
-export const subtract = document.querySelector('[data-key="subtract"]');
-      const add = document.querySelector('[data-key="add"]');
-      const resetButton = document.querySelector('[data-key="reset"]');
-      const counter = document.querySelector('[data-key="number"]');
-      let currentValue = 0;
-      const MIN = -1000;
-      const MAX = 1000;
+const subtract = document.querySelector('[data-key="subtract"]');
+const add = document.querySelector('[data-key="add"]');
+const resetButton = document.querySelector('[data-key="reset"]');
+const counter = document.querySelector('[data-key="number"]');
+let counterValue = 0;
+const MIN = -1000;
+const MAX = 1000;
 
-      const subtractHandler = () => {
-        if (counterValue > MIN) {
-          counterValue--;
-          counter.value = counterValue;
-          add.disabled = false;
-        }
-        if (counterValue === MIN) {
-          subtract.disabled = true;
-        }
-      };
+// This function handles the click event on the subtract button
+const subtractHandler = () => {
+  if (counterValue > MIN) {
+    counterValue--;
+    counter.value = counterValue;
+    add.disabled = false;
+  }
+  if (counterValue === MIN) {
+    subtract.disabled = true;
+  }
+};
 
-      const addHandler = () => {
-        if (counterValue < MAX) {
-          counterValue++;
-          counter.value = counterValue;
-          subtract.disabled = false;
-        }
-        if (counterValue === MAX) {
-          add.disabled = true;
-        }
-      };
-      resetButton.addEventListener('click', () => {
-        resetCounter();
-      });
-      
-      const resetCounter = () => {
-        counter.value = 0;
-        alert('Counter has been reset!');
-        subtract.disabled = false;
-        add.disabled = false;
-      };
+// This function handles the click event on the add button
+const addHandler = () => {
+  if (counterValue < MAX) {
+    counterValue++;
+    counter.value = counterValue;
+    subtract.disabled = false;
+  }
+  if (counterValue === MAX) {
+    add.disabled = true;
+  }
+};
+
+// This function resets the counter to zero and enables both buttons
+const resetCounter = () => {
+  counterValue = 0;
+  counter.value = counterValue;
+  alert('Counter has been reset!');
+  subtract.disabled = false;
+  add.disabled = false;
+};
+
+// Attach click event listeners to each button that call the appropriate function
+subtract.addEventListener('click', subtractHandler);
+add.addEventListener('click', addHandler);
+resetButton.addEventListener('click', resetCounter);
